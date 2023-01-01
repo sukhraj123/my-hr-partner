5d59446a9057baa0c8d821ad625c556f19d10dbd
Keez <74885533+sukhraj123@users.noreply.github.com>
2023-01-01 12:14:35 -0800

    Add files via upload

--- numbers.js
+++ numbers.js
@@ -1,4 +1,11 @@
-var num1 = window.prompt("enter yearly earnings")
-var num1 = window.prompt("Dollars per hour")
-
-document.write(num1 / num2)
+function getBaseLog(x, y) {
+    return Math.log(y) / Math.log(x);
+  }
+  
+  // 2 x 2 x 2 = 8
+  console.log(getBaseLog(2, 8));
+  // expected output: 3
+  
+  // 5 x 5 x 5 x 5 = 625
+  console.log(getBaseLog(5, 625));
+  // expected output: 4
